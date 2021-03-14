065c07ce98a42b2f6acdf6408cfb68b4bd0b9f03
Leo <ronald.mauritsson@gmail.com>
2021-03-14 04:31:45 +0300

    Modified span

--- src/components/TopBarAlert.js
+++ src/components/TopBarAlert.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const TopBarAlert = () => {
   return (
     <div className="topbar__alert">
       <p>
         One of Broken Vessel's core objectives is to enable access to
         counselling services for everyone who can not afford. Please donate to
-        enable us to do that. <span>DONATE</span>
+        enable us to do that.{" "}
+        <span>
+          <Link to="">DONATE</Link>
+        </span>
       </p>
     </div>
   );
